f045be55b22467c902636204d3df0b0ae80caaa1
Drew Chojnowski <drewski@alphaboo.nmsu.edu>
2016-02-12 13:11:31 -0700

    updating exptimecalc

--- exptimecalc/exptime.py
+++ exptimecalc/exptime.py
@@ -33,8 +33,12 @@ wave_k=21900.0  ; width_k=4100.0
 hplanck=6.6260755e-27 # erg s
 cspeed=2.99792458e18  # angstrom/s
 
-def exptime(telescope='nmsu1m',instrument=None,snr=100,wavelength=5500,bandwidth=1000,starmag=22,band='V',
-            magsystem='Vega',seeing=1.0,airmass=1.0,lunarphase=7.0,silent=False):
+"""
+EXPTIME: 
+exposure time calculator
+"""
+def exptime(telescope='nmsu1m',instrument=None,snr=100,wavelength=5500,bandwidth=1000,starmag=22,
+            band='V',magsystem='Vega',seeing=1.0,airmass=1.0,lunarphase=7.0,silent=False):
 
     if magsystem is None:
         choice=raw_input('Enter 1 for Vega mag. system, 2 for AB mag. system: ')
@@ -44,15 +48,10 @@ def exptime(telescope='nmsu1m',instrument=None,snr=100,wavelength=5500,bandwidth
     if telescope=='nmsu1m': mirror_area=get_mirror_area(1,'m')
     if telescope=='3.5m': mirror_area=get_mirror_area(3.5,'m')
 
-    sys_efficiency=get_system_efficiency(wavelength=wavelength,'nmsu1m')
-    atm_trans=get_atmospheric_transmission(wavelength=wavelength)
+    signal=get_signal(exptime=exptime,wavelength=wavelength,bandwidth=bandwidth,starmag=starmag,band=band,
+                      magsystem=magsystem,seeing=seeing,airmass=airmass,lunarphase=lunarphase,silent=silent)
 
-    flux=get_flux(wavelength=wavelength,starmag=starmag,band=band,magsystem=magsystem)
-    int_lambda=0.5*((wavelength+bandwidth/2.)**2-(wavelength-bandwidth/2.)**2)
-
-    s_prime=atm_trans*sys_efficiency*flux*(1./hplanck)*(1./cspeed)*int_lambda
-
-    expt=(snr**2)/(s_prime*mirror_area) # calculate the exposure time
+    expt=(snr**2)/(signal*mirror_area) # calculate the exposure time
 
     if silent is False:
         print '===================================================================='  
@@ -63,22 +62,69 @@ def exptime(telescope='nmsu1m',instrument=None,snr=100,wavelength=5500,bandwidth
 
     return expt
 
-def calculate_snr(exptime=None):
-    it doesnt matter what i do here. will not be correct.
+"""
+CALCULATE_SNR: 
+calculate the expected S/N ratio given an exposure time
+"""
+def calculate_snr(telescope='nmsu1m',instrument=None,exptime=600,starmag=22,band='V',magsystem='Vega',
+                  wavelength=5500,bandwidth=1000,seeing=1.0,airmass=1.0,lunarphase=7.0,aperture=1.0,
+                  readnoise=5.0,platescale=0.5,sky=21,silent=False):
 
-    return nothing
+    signal=get_signal(exptime=exptime,wavelength=wavelength,bandwidth=bandwidth,starmag=starmag,band=band,
+                      magsystem=magsystem,seeing=seeing,airmass=airmass,lunarphase=lunarphase,silent=silent)
+    npix=math.pi*(aperture**2)*(1.0/platescale)
+    apix=math.pi*(aperture**2)
 
+    snr=signal/math.sqrt(signal+(sky*apix)+(npix*(readnoise**2)))
+
+    print signal
+    if silent is False:
+        print '===================================================================='  
+        print 'In a ',exptime,' s exposure of a ',band,'=',starmag,' object, using the '
+        print telescope,' telescope:\n'
+        print '\nS/N = ',snr
+        print '===================================================================='  
+
+    return snr
+
+"""
+GET_SIGNAL: 
+returns the expected photons per second
+"""
+def get_signal(exptime=None,wavelength=None,bandwidth=None,starmag=None,band=None,magsystem=None,
+               seeing=None,airmass=None,lunarphase=None,silent=None):
+
+    sys_efficiency=get_system_efficiency(wavelength=wavelength,telescope='nmsu1m')
+    atm_trans=get_atmospheric_transmission(wavelength=wavelength)
+    flux=get_flux(wavelength=wavelength,starmag=starmag,band=band,magsystem=magsystem)
+    int_lambda=0.5*((wavelength+bandwidth/2.)**2-(wavelength-bandwidth/2.)**2)
+    signal=atm_trans*sys_efficiency*flux*(1./hplanck)*(1./cspeed)*int_lambda
+
+    return signal
+
+"""
+GET_SYSTEM_EFFICIENCY: 
+returns the system efficiency at a given wavelength for a given telescope and instrument.
+"""
 def get_system_efficiency(wavelength=None,telescope=None,instrument=None):
     if telescope=='nmsu1m': se=0.5
     if telescope=='apo3.5m': se=0.5
 
     return se
 
+"""
+GET_ATMOSPHERIC TRANSMISSION: 
+returns the atmospheric transmission at a given wavelength.
+"""
 def get_atmospheric_transmission(wavelength=None):
     at=0.8
 
     return at
 
+"""
+GET_MIRROR_AREA: 
+returns the are of the telescope mirror.
+"""
 def get_mirror_area(msize=None,munits=None):
     if munits=='m': msize=msize*100.0
     if munits=='cm': msize=msize
@@ -86,6 +132,10 @@ def get_mirror_area(msize=None,munits=None):
 
     return ma
 
+"""
+GET_FLUX: 
+returns flux of an object given a magnitude and magnitude system
+"""
 def get_flux(wavelength=None,starmag=None,band=None,magsystem=None):
     if magsystem=='Vega':
         if band=='U': flux=vega_u*(10**(starmag/(-2.5)))
